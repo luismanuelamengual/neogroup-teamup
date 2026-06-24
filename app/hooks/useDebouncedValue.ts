8121dc390afa9a7198a35056f2585685d9383e9f
Luis Amengual <luis.amengual@halopowered.com>
2026-06-24 14:00:19 -0300

    Move debounced value hook on root

--- app/hooks/useDebouncedValue.ts
+++ app/hooks/useDebouncedValue.ts
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react'
 
-/** Returns the given value after it has been stable for `delay` milliseconds. */
 export function useDebouncedValue<T>(value: T, delay = 400): T {
   const [debounced, setDebounced] = useState(value)
 
